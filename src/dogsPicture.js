42fb342a2f0cb262a32fc18a7a03b9323229ec23
Annem jyothi <jyothi@gmail.com>
2023-08-10 22:02:36 +0530

    debugging

--- src/dogsPicture.js
+++ src/dogsPicture.js
@@ -4,31 +4,33 @@ export default function DogPicture() {
   const [picture, setPicture] = useState(
     "https://images.dog.ceo/breeds/terrier-american/n02093428_632.jpg"
   );
+  const [url, setUrl] = useState("https://dog.ceo/api/breeds/image/random");
   // const [next, setNext] = useState(picture);
   const nextimage = async (e) => {
-    const response = await axios.get("https://dog.ceo/api/breeds/image/random");
+    const response = await axios.get(url);
     // console.log(response.data.message);
     setPicture(response.data.message);
   };
   const selopt = async (e) => {
-    if (e.target.value !== "random") {
-      const response = await axios.get(
-        `https://dog.ceo/api/breed/${e.target.value}/images/random`
-      );
-      setPicture(response.data.message);
-      // setNext();
+    if (e.target.value === "random") {
+      setUrl("https://dog.ceo/api/breeds/image/random");
     } else {
-      const response = await axios.get(
-        "https://dog.ceo/api/breeds/image/random"
-      );
-      // console.log(response.data.message);
-      setPicture(response.data.message);
+      setUrl(`https://dog.ceo/api/breed/${e.target.value}/images/random`);
     }
+    const response = await axios.get(url);
+    setPicture(response.data.message);
+    // setNext();
   };
+
   return (
     <div>
       <h2>Select a breed:</h2>
-      <select name="dogs" onChange={selopt}>
+      <select
+        name="dogs"
+        onChange={(e) => {
+          selopt(e);
+        }}
+      >
         <option value="random">random</option>
         <option value="beagle">beagle</option>
         <option value="boxer">boxer</option>
@@ -36,7 +38,7 @@ export default function DogPicture() {
         <option value="husky">husky</option>
       </select>
       <br></br>
-      <input type="image" src={picture} alt="dog pictures" />
+      <input type="image" id="picture" src={picture} alt="dog pictures" />
       <br></br>
       <button onClick={nextimage}>next</button>
     </div>
